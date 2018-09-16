017ad2117c8709f4353bbc6d51defe9fa6486fe5
huangxb92 <490460397@qq.com>
2018-09-16 16:06:55 +0800

    Signed-off-by: huangxb92 <490460397@qq.com>

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@RequestMapping("/hello")
 	public String hello(){
-		return "hello1";
+		return "hello";
 	}
 }
