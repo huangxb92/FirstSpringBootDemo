930c4ecd684da6d9d07683a486bc58f3b55fda2a
huangxb92 <490460397@qq.com>
2018-09-16 16:07:38 +0800

    第三次提交

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@RequestMapping("/hello")
 	public String hello(){
-		return "hello";
+		return "hello1";
 	}
 }
